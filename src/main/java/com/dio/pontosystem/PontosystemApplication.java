a49c1906589b7f3923fe685947c98bce00d5a41c
Bernard Braun <bernard.sbraun@gmail.com>
2021-08-02 13:51:46 -0300

    Commit inicial

---
+++ src/main/java/com/dio/pontosystem/PontosystemApplication.java
@@ -0,0 +1,13 @@
+package com.dio.pontosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontosystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontosystemApplication.class, args);
+	}
+
+}
