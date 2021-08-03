65b577e8e71bd56a7bb1905f93273aa95b78f3e7
Bernard Braun <bernard.sbraun@gmail.com>
2021-08-03 08:25:00 -0300

    .

---
+++ src/main/java/com/dio/pontosystem/model/Movimentacao.java
@@ -0,0 +1,40 @@
+package com.dio.pontosystem.model;
+
+import lombok.*;
+
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+
+import javax.persistence.OneToMany;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class Movimentacao {
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @EqualsAndHashCode
+    @Embeddable
+    public class MovimentacaoId implements Serializable {
+        private long idMovimento;
+        private long idUsuario;
+    }
+
+    @EmbeddedId
+    private MovimentacaoId id;
+    private LocalDateTime dataEntrada;
+    private LocalDateTime dataSaida;
+    private BigDecimal periodo;
+    @OneToMany
+    private Ocorrencia ocorrencia;
+    @OneToMany
+    private Calendario calendario;
+
+}
